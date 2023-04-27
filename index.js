ab5db999d4a4b3b9de7350b6bd595d29cf29c0d9
rinkuahamed <ringkuali18@gmail.com>
2023-04-27 10:41:53 +0600

    Dragon news server

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+const categories = require("./categories/categories.json");
+const news = require("./news/news.json");
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Hello I am working");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const singleNews = news.find((nw) => nw._id === id);
+  res.send(singleNews);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
